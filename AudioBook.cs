d537d2a4ba34a61d15dd760d445f99b60ed9a965
Eddie C. <edimc2000@gmail.com>
2025-11-23 21:12:17 -0600

    added xml docs  for audiobook.cs

--- AudioBook.cs
+++ AudioBook.cs
@@ -1,33 +1,49 @@
 ﻿namespace Library;
 
+/// <summary>Represents an audiobook in the library system</summary>
+/// <remarks>
+/// <para>Author: Eddie C.</para>
+/// <para>Version: 1.0</para>
+/// <para>Since: 2025-11-21</para>
+/// </remarks>
 internal class AudioBook : IBook
 {
+    /// <summary>Gets or sets the book title</summary>
+    public string Title { get; set; }
+
+    /// <summary>Gets or sets the book title</summary>
+    public bool IsAvailable { get; set; }
+
+
+    /// <summary>Gets or sets the book location</summary>
+    public string Location { get; set; }
+
+    /// <summary>Initializes a new audiobook</summary>
+    /// <param name="title">The title of the book</param>
+    public AudioBook(string title)
+    {
+        Title = title;
+        Location = "Web";
+        IsAvailable = true;
+    }
+
+    /// <summary>Marks the book as borrowed</summary>
     public void MarkAsBorrowed()
     {
         IsAvailable = false;
     }
 
+    /// <summary>Marks the book as returned</summary>
     public void MarkAsReturned()
 
     {
         IsAvailable = true;
     }
 
+    /// <summary>Gets the current location</summary>
+    /// <returns>The book location</returns>
     public string GetLocation()
     {
-        return Location; 
-    }
-
-    public string? Title { get; set; }
-
-    public string Location { get; set; }
-    public bool IsAvailable { get; set; }
-
-
-    public AudioBook(string title)
-    {
-        Title = title;
-        Location = "Web";
-        IsAvailable = true;
+        return Location;
     }
 }
